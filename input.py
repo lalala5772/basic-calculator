2afb18a3e94c57ea39c43c146eb23356a5240974
JONGMIN <pjm0409@naver.com>
2023-11-09 21:30:03 +0900

    feature:input class

---
+++ input.py
@@ -0,0 +1,20 @@
+class CalculatorInput:
+    def __init__(self, input_str):
+        self.input_str = input_str
+
+    def cal_input(self):
+        # 정수인지 판단 - 양수 or 음수
+        if self.input_str.isdigit() or (self.input_str[1:].isdigit() and self.input_str[0] == '-'):
+            return "Integer"
+        # 기호인지 판단
+        elif self.input_str in ['+', '-', '*']:
+            return "Operator"
+        # 그외 판단
+        else:
+            return "Other"
+
+# 사용 예시
+user_input = input("숫자 또는 기호를 입력하세요: ")
+cal_class = CalculatorInput(user_input)
+result = cal_class.cal_input()
+print(f"입력된 값은 {result}입니다.")
