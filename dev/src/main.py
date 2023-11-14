c325986dced86fc977ea6d69f39b38143ea37de9
mir <65425885+lalala5772@users.noreply.github.com>
2023-11-14 09:30:01 +0900

    Update main.py
    main 플로우 업데이트

--- dev/src/main.py
+++ dev/src/main.py
@@ -1,58 +1,40 @@
 import sys
-import time
-result = None # 결과 값을 저장할 변수, 초기에는 아무 값도 없음
-operator = ''  # 현재 선택된 연산자를 저장할 변수, 초기에는 없음
+from incalculate import Incalculate
+from calculation import Calculation
+from easterEgg import EasterEgg
+from input import CalculatorInput
+from isError import calculatorIsError
 
-class EasterEgg:
-    def __init__(self):
-        pass
-    def checkEasterEgg(self, expression): 
-        #input이 0이라면 프로그램 종료(이스터에그 기능2)
-        if expression == "0":
-            for i in range(3, 0, -1):
-                print(f"{i}초 후 프로그램을 종료합니다")
-                time.sleep(1)
-            print("프로그램 종료")
-            sys.exit()
-        #input이 정해진 학번이라면 학생 이름 출력(이스터에그 기능1)    
-        else:                   
-            students = {
-                '201918757': '강성택',
-                '201918777': '박종민',
-                '201912430': '조민서',
-                '201912431': '조승호',
-                '201917604': '이관호',
-                '201912393': '이미르',
-                '202146712': '박용수'
-            }
-            student_name = students.get(expression)
-            if student_name:
-                print(student_name)
-            else:
-                pass           
-easter_egg = EasterEgg() #이스터 에그 확인용 임시코드(나중에 코드 리팩토링 할때 수정부탁)
-while True:
-    expression = input("수식을 입력하세요 (종료하려면 '=' 입력): ")
-    easter_egg.checkEasterEgg(expression) #이스터 에그 확인용 임시코드(나중에 코드 리팩토링 할때 수정부탁)
-    # 입력이 '=' 면 반복문 종료
-    if expression == "=":
-        break
-    # 연산자 +, -, * 이고, 초기 값이 존재하면 연산자 초기화
-    if expression in ('+', '-', '*'):
-        if result is not None:
-            operator = expression
-    else:
-        num = int(expression)
-        # result  인 경우 result = num
-        if result is None:
-            result = num
 
-        # result 가 있고, 연산자가 들어오면 해당 연산
-        else:
-            if operator == '+':
-                result += num
-            elif operator == '-':
-                result -= num
-            elif operator == '*':
-                result *= num
-print(f"결과: {result}")
+if __name__ == "__main__":
+
+  before_input = ''
+
+  while True:
+
+    #입력받기
+    user_input = input("숫자 또는 기호를 입력하세요: ")
+
+    #필요한 객체 생성
+    calculator_input = CalculatorInput(user_input) #오류 유무 확인
+    input_error_message = calculatorIsError() #오류메세지
+    #calculator = Calculation() #계산
+    #incalculator = Incalculate()
+    #easteregg = easterEgg() -> input에서 처리하기로
+
+    input_check = calculator_input.cal_input()
+
+    if input_check != "Equal"  :
+      #calculate 실행 및 출력 
+      break #종료
+
+    #에러 유무 확인
+    if input_check == 'Other' :
+      #error 메세지 출력
+      #beforeInput은 함수 선언할 때 전달 -> but 어떤 함수가 호출될지는 파일 안에서 판별 , 외부에서 언제 어떻게 전달? 
+
+    else: 
+      #그 외에 값은 calculate?? 대체 어디에 넘기라는건지
+    
+    before_input = user_input
+
