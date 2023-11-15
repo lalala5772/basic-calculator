e19ed562d22b00cc4e4987a6711c354d38d194e6
minseo  jo <liging12@naver.com>
2023-11-15 23:56:18 +0900

    feat: basic_code 기능 추가

---
+++ dev/src/basic_code.py
@@ -0,0 +1,19 @@
+import time
+
+class Calculator:
+
+class Validator:
+
+class EasterEgg:
+
+class InputView:
+
+class OutputView:
+
+class InputCheck:
+
+class CalculatorController:
+
+if __name__ == "__main__":
+    calculatorController = CalculatorController()
+    calculatorController.run()
