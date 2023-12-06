2ab06b80d2890e005259db44c2bdebeb36b5979a
minseo Jo <liging12@naver.com>
2023-12-07 05:54:08 +0900

    add chool_anniversary_check

--- dev/src/inputcheck.py
+++ dev/src/inputcheck.py
@@ -7,23 +7,30 @@ class InputCheck:
         self.easterEgg = EasterEgg()
         self.outputView = OutputView()
     
-    def input_check(self):
+    def check(self):
         final_expression = []
         
         while True:
-            expression = self.inputView.readLine() # 입력 받아오기
+            expression = self.inputView.read_date() # 입력 받아오기
             final_expression.append(expression)
 
-            if self.easterEgg.isCountdownEasterEgg(expression): # 카운트 다운 이스터에그 체크
-                easterEggMessage, timer, countdown_step = self.easterEgg.startCountdownEasterEgg()
-                self.outputView.countdownPrint(easterEggMessage, timer, countdown_step) # 카운트다운 이스터에그 출력
+            if self.easterEgg.is_countdown_easter_egg(expression): # 카운트 다운 이스터에그 체크
+                easterEggMessage, timer, countdown_step = self.easterEgg.start_countdown_easter_egg()
+                self.outputView.print_count_down_easter_egg(easterEggMessage, timer, countdown_step) # 카운트다운 이스터에그 출력
                 return
 
-            elif self.easterEgg.isStudentIdEasterEgg(expression): # 학번 이스터에그 체크
-                self.outputView.print(self.easterEgg.startStudentIdEasterEgg(expression)) # 학번 이스터에그 출력
+            if self.easterEgg.is_student_id_easter_egg(expression): # 학번 이스터에그 체크
+                self.outputView.print_easter_egg(self.easterEgg.start_student_id_easter_egg(expression)) # 학번 이스터에그 출력
                 return
 
-            if expression == "=": # 입력이 '=' 면 반복문 종료
+            if self.easterEgg.is_school_anniversary(expression): # 개교 기념일 체크
+                self.outputView.print_easter_egg(self.easterEgg.start_school_anniversary()) # 개교 기념일 이스터에그 출력
+                return
+
+            if self.is_equal_sign(expression): # 입력이 '=' 면 반복문 종료
                 break
         
         return final_expression
+
+    def is_equal_sign(self, expression):
+        return expression == '='
