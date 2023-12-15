8c6daf927b108e1f254f830757127e89b099d3de
jongmiiin <101557261+jongmiiin@users.noreply.github.com>
2023-12-16 00:32:58 +0900

    Update calculatorcontroller.py

--- dev/src/calculatorcontroller.py
+++ dev/src/calculatorcontroller.py
@@ -18,10 +18,11 @@ class CalculatorController:
                 if self.validator.is_factorial_expression(final_expression): # '!' 가 포함된 경우 팩토리얼 검증, 검증 성공하면 팩토리얼 계산
                     self.validator.factorial_validate(final_expression)
                     calculation_result = self.calculator.factorial_calculate(final_expression)
+                    self.outputView.print_calulation_result('= ' + str(calculation_result))  # 계산 결과 출력
                 else:
                     self.validator.default_validate(final_expression) # '!' 가 포함되 있지 않은 기본 계산기 검증, 검증 성공하면 기본 계산기 계산
                     calculation_result = self.calculator.calculate(final_expression)
-                self.outputView.print_calulation_result(calculation_result)  # 이외의 경우, 계산 결과 출력
+                    self.outputView.print_calulation_result(calculation_result)  # 계산 결과 출력
 
             except FactorialException as exception:
                 self.outputView.print_error_message(exception)
